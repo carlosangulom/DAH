59e972decba841472ce1db7740f0a0bbcc27c5c9
carlosangulom <49261785+carlosangulom@users.noreply.github.com>
2022-10-05 03:02:10 -0600

    vite+react

---
+++ react-vite/mi_proyecto_vite/src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Expenses from "./routes/Expenses";
+import Invoices from "./routes/Invoices";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
